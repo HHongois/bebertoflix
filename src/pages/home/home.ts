dd63f1fe4ae6b7240f5af638a47ea62290571cfa
HONGOIS Hubert <hubert.hongois@gmail.com>
2018-11-30 10:19:52 +0100

    search et liste ok

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { ServiceDonneesProvider } from './../../providers/service-donnees/service-donnees';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -7,8 +8,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+public data:Array<Object> = [];
+public entree = ''; 
 
+  constructor(public navCtrl: NavController,public donnees:ServiceDonneesProvider) {
+
+    this.donnees.getMovies().subscribe((listMovies) =>{
+      this.data = listMovies;
+    });
+  }
+
+  public search(){
+    this.donnees.searchMovies(this.entree).subscribe((listMovies) => {
+      this.data = listMovies;
+    });
+  }
+
+  itemTapped(event, exemple) {
+    this.navCtrl.push(HomePage, {
+      item: exemple
+    });
   }
 
 }
---
+++ src/providers/service-donnees/service-donnees.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {map} from 'rxjs/operators';
+
+/*
+  Generated class for the ServiceDonneesProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ServiceDonneesProvider {
+
+  constructor(public http: HttpClient) {
+    console.log('Hello ServiceDonneesProvider Provider');
+  }
+
+  public getMovies(){
+    return this.http.get('http://www.omdbapi.com/?s=bat&apikey=75522b56').pipe(
+        map(response => response['Search'] )
+      );  
+  }
+  public searchMovies(ex:string){
+    return this.http.get('http://www.omdbapi.com/?s='+ex+'&apikey=75522b56').pipe(
+      map(response => response['Search'] )
+    );
+  }
+
+}
